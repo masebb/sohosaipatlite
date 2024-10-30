08664f39b62ceecf3d38d95c6a86d979348bd7b6
whtacotton <whatacotton@gmail.com>
2024-10-30 16:20:44 +0900

    revail functions

--- src/main.go
+++ src/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"log"
 	"net"
+	"net/http"
+	"strconv"
 
 	"time"
-
-	"github.com/sohosai/sopolight/src/sohosaiPatliteLib"
 )
 
 func main() {
-	p := sohosaiPatliteLib.Patlite{
+	p := Patlite{
 		Ipaddr: net.ParseIP("192.168.10.1"),
 		Red:    On,
 		Yellow: On,
@@ -35,5 +36,58 @@ func main() {
 	}
 }
 
-var Off = sohosaiPatliteLib.PatlitePattern{Name: "off", ID: 0}
-var On = sohosaiPatliteLib.PatlitePattern{Name: "on", ID: 1}
+var Off = PatlitePattern{Name: "off", ID: 0}
+var On = PatlitePattern{Name: "on", ID: 1}
+
+type Patlite struct {
+	Ipaddr net.IP
+	Red    PatlitePattern
+	Yellow PatlitePattern
+	Green  PatlitePattern
+	Buzzer PatlitePattern
+}
+
+func (p *Patlite) SetPatlite(red PatlitePattern, yellow PatlitePattern, green PatlitePattern, buzzer PatlitePattern) {
+	p.Red = red
+	p.Yellow = yellow
+	p.Green = green
+	p.Buzzer = buzzer
+	p.sendAlertToPatlite()
+}
+
+func (p *Patlite) SendClearToPatlite() {
+	url := "http://" +
+		p.Ipaddr.String() +
+		"/api/control?clear=1"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println("Error at clear: ", err)
+	}
+	defer resp.Body.Close()
+}
+
+type PatlitePattern struct {
+	Name string
+	ID   int
+}
+
+func (p *Patlite) sendAlertToPatlite() {
+	url := "http://" +
+		p.Ipaddr.String() +
+		"/api/control?alert=" +
+		strconv.Itoa(p.Red.ID) +
+		strconv.Itoa(p.Yellow.ID) +
+		strconv.Itoa(p.Green.ID) +
+		"0" + // 青は未使用
+		"0" + // 白は未使用
+		strconv.Itoa(p.Buzzer.ID)
+
+	log.Println("GET request to ", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println("Error at set: ", err)
+	}
+	defer resp.Body.Close()
+
+	log.Println(resp)
+}
